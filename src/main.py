225fb7c2c73bcb6db494a8fbd5ed37d3307f7ed3
ZAAFHachemrachid <rachidzaaf9@gmail.com>
2025-01-10 03:28:32 +0100

    style: Change background color to black

--- src/main.py
+++ src/main.py
@@ -22,7 +22,7 @@ class LibraryApp(ctk.CTk):
 
         self.title("Library Management System")
         self.geometry("800x600")
-        self.bg_color = "#f0f0f0"
+        self.bg_color = "#000000"
 
         # Configure grid layout (1x2)
         self.grid_columnconfigure(1, weight=1)
